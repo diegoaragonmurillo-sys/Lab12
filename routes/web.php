d4360213d46e5e1aaddae9c8b1b59d65151d76cb
einarsejje-666 <einar.sejje@tecsup.edu.pe>
2025-11-05 00:24:28 -0500

    Duplicate posts resource route with a comment

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('posts', PostController::class);
+# Este también es un comentario
