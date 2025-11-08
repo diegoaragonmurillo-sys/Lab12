5a69346330ab6f3b51117652f9bec19293f1b8de
einarsejje-666 <einar.sejje@tecsup.edu.pe>
2025-11-08 01:01:11 -0500

    agregacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::resource('posts', PostController::class);
+Route::post('/posts/{post}/comments', [CommentController::class, 'store'])
+    ->name('comments.store'); // <-- Ahora no tiene restricción
+Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+
+
+Route::put('/comments/{comment}', [CommentController::class, 'update'])
+    ->name('comments.update')
+    ->middleware('auth'); 
+
+Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])
+    ->name('comments.destroy')
+    ->middleware('auth');
