c0ac472edbd2f2d2d191bc2a451867bf92f4efac
diegoaragonmurillo-sys <diego.aragon@tecsup.edu.pe>
2025-11-12 23:02:33 -0500

    Modificacion de models en recordatorios

--- app/Models/Recordatorio.php
+++ app/Models/Recordatorio.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Recordatorio extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nota_id', 'fecha_vencimiento', 'completado'];
+
+    // Relación: Recordatorio pertenece a una nota
+    public function nota()
+    {
+        return $this->belongsTo(Nota::class);
+    }
 }
